22586ed2823b3272fb8b7bd9385e40a7aeac66f4
Andrew Davis <andrew@lightcorp.net>
2013-10-31 06:59:03 -0500

    chopping wood

---
+++ meatparties.js
@@ -0,0 +1,6 @@
+t var PATH = 'https://raw.github.com/diffalot/meatparties/gh-pages/';
+//document.body.appendChild(document.createElement('script')).src=PATH+'speakGenerator.js';
+
+$.getJSON( PATH + 'guestlist.json', function (data) {
+	console.log(data);
+});
