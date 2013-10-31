63875a291174b648c6feb1eab8236301d1bb6932
Andrew Davis <andrew@lightcorp.net>
2013-10-31 08:20:43 -0500

    simplify more?w

--- meatparties.js
+++ meatparties.js
@@ -1,12 +1,10 @@
 var PATH = 'https://raw.github.com/diffalot/meatparties/gh-pages/';
 //document.body.appendChild(document.createElement('script')).src=PATH+'speakGenerator.js';
 
-var logger = function (data) {
-	console.dir(data);
-};
-
 $.ajax({
 	url: PATH + 'guestlist.json',
 	dataType: 'jsonp',
-	success: logger
+	success: function (data) {
+		console.dir(data);
+	}
 });
