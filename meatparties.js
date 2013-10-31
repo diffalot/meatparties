a8119702147517aab9b888846f14c254f68dd5a1
Andrew Davis <andrew@lightcorp.net>
2013-10-31 07:01:16 -0500

    bad letter, get out!

--- meatparties.js
+++ meatparties.js
@@ -1,4 +1,4 @@
-t var PATH = 'https://raw.github.com/diffalot/meatparties/gh-pages/';
+var PATH = 'https://raw.github.com/diffalot/meatparties/gh-pages/';
 //document.body.appendChild(document.createElement('script')).src=PATH+'speakGenerator.js';
 
 $.getJSON( PATH + 'guestlist.json', function (data) {
